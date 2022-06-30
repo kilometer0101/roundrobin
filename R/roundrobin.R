5dba3869cff6a9e39fd93e89c6d85ccd5da3d613
km <>
2022-06-30 10:39:20 +0900

    initial commit

---
+++ R/roundrobin.R
@@ -0,0 +1,15 @@
+
+roundrobin <- function(data, key){
+  data_nest <-
+    data %>%
+    dplyr::rename(key = key) %>%
+    dplyr::group_nest(key)
+
+  data_nest$key %>%
+    base::expand.grid(., .) %>%
+    tibble::as_tibble() %>%
+    dplyr::left_join(data_nest %>% dplyr::rename(Var1 = key),
+                     by = "Var1")%>%
+    dplyr::left_join(data_nest %>% dplyr::rename(Var2 = key),
+                     by = "Var2")
+}
