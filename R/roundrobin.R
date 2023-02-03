c2e9dcf09054b35d7478274422780bc03716ed9e
km <>
2023-02-03 09:37:08 +0900

    update

--- R/roundrobin.R
+++ R/roundrobin.R
@@ -6,13 +6,22 @@
 #' @param data target data.frame or tibble
 #' @param key target key for combination calcuration
 #' @export
-roundrobin <- function(data, key){
+roundrobin <- function(data, key, unclass = FALSE){
   data_nest <-
     data %>%
     dplyr::rename(key = key) %>%
     dplyr::group_nest(key)
 
-  base::expand.grid(data_nest$key, data_nest$key) %>%
+  grid <-
+    base::expand.grid(data_nest$key, data_nest$key)
+
+  if(unclass){
+    grid <-
+      grid %>%
+      subset(unclass(Var1) < unclass(Var2))
+  }
+
+  grid %>%
     tibble::as_tibble() %>%
     dplyr::left_join(data_nest %>% dplyr::rename(Var1 = key),
                      by = "Var1")%>%
