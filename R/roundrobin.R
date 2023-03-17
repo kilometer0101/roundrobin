61bfe5433550a143cb206ee08fbd2969934755ab
km <>
2023-03-17 14:16:02 +0900

    update

--- R/roundrobin.R
+++ R/roundrobin.R
@@ -7,7 +7,7 @@
 #' @param key target key for combination calcuration
 #' @param unclass whether to make them a unique pair or not
 #' @export
-roundrobin <- function(data, key, unclass = FALSE){
+roundrobin <- function(data, key, unclass = FALSE, rename = TRUE){
   data_nest <-
     data %>%
     dplyr::rename(key = key) %>%
@@ -22,10 +22,25 @@ roundrobin <- function(data, key, unclass = FALSE){
       base::subset(base::unclass(Var1) < base::unclass(Var2))
   }
 
-  grid %>%
+
+  result <-
+    grid %>%
     tibble::as_tibble() %>%
     dplyr::left_join(data_nest %>% dplyr::rename(Var1 = key),
-                     by = "Var1")%>%
+                     by = "Var1") %>%
     dplyr::left_join(data_nest %>% dplyr::rename(Var2 = key),
                      by = "Var2")
+
+
+  if(rename == TRUE){
+    tag_x <- str_c(key, ".x")
+    tag_y <- str_c(key, ".y")
+
+    result <-
+      result %>%
+      rename(!!tag_x := Var1) %>%
+      rename(!!tag_y := Var2)
+  }
+
+  return(result)
 }
